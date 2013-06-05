4bb714efa87eb9d21e06b7ec767cf2901adf2fd6
Jason Lunz <jason.lunz@gmail.com>
2013-06-05 10:08:40 -0700

    data: add support for the 'x' (binary) type in amqp tables

--- tests/data_tests.py
+++ tests/data_tests.py
@@ -16,17 +16,23 @@ from pika import exceptions
 
 class DataTests(unittest.TestCase):
 
-    FIELD_TBL_ENCODED = ('\x00\x00\x00\xbb\x07longvall\x00\x00\x00\x006e&U'
-                         '\x06intvalI\x00\x00\x00\x01\x07dictvalF\x00\x00'
-                         '\x00\x0c\x03fooS\x00\x00\x00\x03bar\x07unicodeS'
-                         '\x00\x00\x00\x08utf8=\xe2\x9c\x93\x05arrayA\x00'
-                         '\x00\x00\x0fI\x00\x00\x00\x01I\x00\x00\x00\x02I'
-                         '\x00\x00\x00\x03\x04nullV\x06strvalS\x00\x00\x00'
-                         '\x04Test\x0ctimestampvalT\x00\x00\x00\x00Ec)\x92'
-                         '\x07decimalD\x02\x00\x00\x01:\x07boolvalt\x01'
-                         '\x0bdecimal_tooD\x00\x00\x00\x00d')
+    FIELD_TBL_ENCODED = ('\x00\x00\x00\xce'
+            '\x07longvall\x00\x00\x00\x006e&U'
+            '\x06intvalI\x00\x00\x00\x01'
+            '\x07boolvalt\x01'
+            '\x07unicodeS\x00\x00\x00\x08utf8=\xe2\x9c\x93'
+            '\x05arrayA\x00\x00\x00\x0fI\x00\x00\x00\x01I\x00\x00\x00\x02I\x00\x00\x00\x03'
+            '\x04nullV'
+            '\x06''strvalS\x00\x00\x00\x04Test'
+            '\x0ctimestampvalT\x00\x00\x00\x00Ec)\x92'
+            '\x07decimalD\x02\x00\x00\x01:'
+            '\x07dictvalF\x00\x00\x00\x0c\x03fooS\x00\x00\x00\x03bar'
+            '\x0bdecimal_tooD\x00\x00\x00\x00d'
+            '\x06binvalx\x00\x00\x00\x07bindata')
+
     FIELD_TBL_VALUE = {'intval': 1,
                        'strval': 'Test',
+                       'binval': data.binary('bindata'),
                        'boolval': True,
                        'unicode': u'utf8=✓',
                        'decimal': decimal.Decimal('3.14'),
@@ -46,7 +52,7 @@ class DataTests(unittest.TestCase):
     def test_encode_table_bytes(self):
         result = []
         byte_count = data.encode_table(result, self.FIELD_TBL_VALUE)
-        self.assertEqual(byte_count, 191)
+        self.assertEqual(byte_count, len(self.FIELD_TBL_ENCODED))
 
     def test_decode_table(self):
         value, byte_count = data.decode_table(self.FIELD_TBL_ENCODED, 0)
@@ -54,7 +60,7 @@ class DataTests(unittest.TestCase):
 
     def test_decode_table_bytes(self):
         value, byte_count = data.decode_table(self.FIELD_TBL_ENCODED, 0)
-        self.assertEqual(byte_count, 191)
+        self.assertEqual(byte_count, len(self.FIELD_TBL_ENCODED))
 
     def test_encode_raises(self):
         self.assertRaises(exceptions.UnspportedAMQPFieldException,
